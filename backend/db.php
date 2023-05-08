967de3508efd164ecf7184c20c93a8d8faae7f31
onlinehk <18662909+onlinehk@users.noreply.github.com>
2023-05-08 14:23:22 +0800

    support mysqli

--- backend/db.php
+++ backend/db.php
@@ -1,30 +1,40 @@
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>LAMP - Docker</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>LAMP - Docker</title>
 </head>
 
 <body>
-<h1>LAMP - Docker</h1>
-<?php
-$dsn = 'mysql:host=mysql; dbname=testdb';
-$user = 'root';
-$password = 'admin';
+	<h1>LAMP - Docker</h1>
+	<?php
+	// 定義數據庫連接參數
+	$servername = "mysql";
+	$username = "root";
+	$password = "admin";
+	$dbname = "testdb";
 
-try {
-	$pdo = new PDO($dsn, $user, $password);
-} catch (PDOException $e) {
-	echo 'Connection failed: '.$e->getMessage();
-}
+	// 建立數據庫連接
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-$stmt = $pdo -> query("SELECT * FROM people");
-$results = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+	// 執行 SELECT 查詢
+	$sql = "SELECT * FROM people";
+	$result = mysqli_query($conn, $sql);
 
-foreach($results as $result) {
-	foreach($result as $key => $value) {
-		echo $key.": ".$value."<br />";
+	// 檢查查詢結果是否有數據
+	if (mysqli_num_rows($result) > 0) {
+		// 輸出每一行數據
+		while ($row = mysqli_fetch_assoc($result)) {
+			foreach ($row as $key => $value) {
+				echo $key . ": " . $value . "<br />";
+			}
+			echo "<hr />";
+		}
+	} else {
+		echo "0 results";
 	}
-	echo "<hr />";
-}
-?>
+
+	// 關閉數據庫連接
+	mysqli_close($conn);
+	?>
 </body>
+
 </html>
